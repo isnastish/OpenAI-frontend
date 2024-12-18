991d18e17bab50402f2f9540e95d02af667675f2
isnastish <paukden12@gmail.com>
2024-12-18 11:46:59 +0100

    added first name and last name states

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,29 +1,53 @@
 import { Fragment, useState } from "react";
 
+function Input() {
+  return <Fragment></Fragment>;
+}
+
 export default function Signup() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredFirstName, setEnteredFirstName] = useState("");
+  const [enteredLastName, setEnteredLastName] = useState("");
 
   const handleEmailSubmit = (event) => setEnteredEmail(event.target.value);
   const handlePasswordSubmit = (event) =>
     setEnteredPassword(event.target.value);
 
+  const firstNameSubmitHandler = (event) =>
+    setEnteredFirstName(event.target.value);
+  const lastNameSubmitHandler = (event) =>
+    setEnteredLastName(event.target.value);
+
+  const clearSubmittedDataHandler = () => {
+    setEnteredEmail("");
+    setEnteredPassword("");
+    setEnteredFirstName("");
+    setEnteredLastName("");
+  };
+
   function handleFormSubmit(event) {
     event.preventDefault();
 
-    if (enteredEmail !== "" && enteredPassword !== "") {
+    if (
+      enteredEmail !== "" &&
+      enteredPassword !== "" &&
+      enteredFirstName !== "" &&
+      enteredLastName !== ""
+    ) {
       console.log(
-        "submitted email: " + enteredEmail + "password: " + enteredPassword
+        "submitted email: " + enteredEmail + "\npassword: " + enteredPassword + "\nfirstName: " + enteredFirstName + "\nlastName: " + enteredLastName
       );
     }
 
     // TODO: Here we have to redirect to /openai page.
     // So we can make requests to Openai model
 
-    // reset data
-    setEnteredEmail("");
-    setEnteredPassword("");
+    clearSubmittedDataHandler();
   }
+
+  // TODO: There is some repetition with an input,
+  // it has to be moved out into a separte component.
 
   return (
     <Fragment>
@@ -48,6 +72,26 @@ export default function Signup() {
               name="password"
               onChange={handlePasswordSubmit}
               value={enteredPassword}
+            ></input>
+          </div>
+          <div className="control no-margin">
+            <label htmlFor="password">FirstName </label>
+            <input
+              id="first-name"
+              type="first-name"
+              name="first-name"
+              onChange={firstNameSubmitHandler}
+              value={enteredFirstName}
+            ></input>
+          </div>
+          <div className="control no-margin">
+            <label htmlFor="password">LastName </label>
+            <input
+              id="last-name"
+              type="last-name"
+              name="last-name"
+              onChange={lastNameSubmitHandler}
+              value={enteredLastName}
             ></input>
           </div>
         </div>
